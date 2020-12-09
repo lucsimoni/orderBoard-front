58ae27782e655cbbdbbeb16491b96ee58090a42b
lucsimoni <contact.lucsimoni@gmail.com>
2020-12-09 11:31:59 +0100

    subject rxjs

--- src/app/services/storage/session-storage.service.ts
+++ src/app/services/storage/session-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@angular/core';
 import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { Observable, Subject } from 'rxjs';
 
 const STORAGE_LANG = 'storage_lang';
 
@@ -7,6 +8,8 @@ const STORAGE_LANG = 'storage_lang';
   providedIn: 'root'
 })
 export class SessionStorageService {
+
+  private storageSub = new Subject<any>();
 
   constructor(
     @Inject(SESSION_STORAGE)
@@ -47,6 +50,38 @@ export class SessionStorageService {
       return;
     else
       this.setLanguage(lang);
+  }
+
+  /*** SUBJECT MODIFICATION EN SESSION ET REPERCUSION DANS TOUS LES COMPOSANTS QUI L UTILISENT ***/
+  /**
+   * DANS LE TS DES COMPOSANTS
+   * public list: string[];
+   * ngOnInit {
+   *  this.list = this.sessionStorageService.getAnInfo();
+   *  this.sessionStorageService.watchStorage().subscribe((data:any) => {
+   *    this.list = data;
+   *  })
+   * }
+   */
+  
+  watchStorage(): Observable<any> {
+    return this.storageSub.asObservable();
+  }
+
+  setAnInfo(info: string) {
+    const listInfos: string[] = this.getAnInfo() || [];
+    listInfos.push(info);
+    this.storage.set('anInfo', listInfos);
+    this.storageSub.next(listInfos) //Ou 'modifié'
+  }
+
+  getAnInfo(): string[] {
+    return this.storage.get('anInfo');
+  }
+
+  clearAnInfo(): void {
+    this.storage.set("anInfo", []);
+    this.storageSub.next([]);
   }
 
 }
